be912367e7d02323b509967c0497efdfb1e51698
Sascha Doemer <Sascha.Doemer@LMIS.de>
2018-06-18 22:37:50 +0200

    Draft.

--- protobuf-decoder/src/main/java/de/saschadoemer/agrirouter/protobuf/decoder/impl/MessageOuterClassDecoder.java
+++ protobuf-decoder/src/main/java/de/saschadoemer/agrirouter/protobuf/decoder/impl/MessageOuterClassDecoder.java
@@ -2,9 +2,10 @@ package de.saschadoemer.agrirouter.protobuf.decoder.impl;
 
 import agrirouter.commons.MessageOuterClass;
 import com.google.protobuf.ByteString;
-import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
 import de.saschadoemer.agrirouter.protobuf.decoder.Decoder;
 
+import java.io.IOException;
 import java.util.Optional;
 
 public class MessageOuterClassDecoder implements Decoder {
@@ -12,8 +13,10 @@ public class MessageOuterClassDecoder implements Decoder {
     public Optional<String> safeDecode(String utf8) {
         try {
             MessageOuterClass.Message message = MessageOuterClass.Message.parseFrom(ByteString.copyFromUtf8(utf8));
-            JsonFormat
-        } catch (InvalidProtocolBufferException e) {
+            StringBuilder output = new StringBuilder();
+            JsonFormat.printer().appendTo(message, output);
+            return Optional.of(output.toString());
+        } catch (IOException e) {
             // NOP
         }
         return Optional.empty();
---
+++ protobuf-decoder/src/test/java/de/saschadoemer/agrirouter/protobuf/decoder/impl/MessageOuterClassDecoderTest.java
@@ -0,0 +1,12 @@
+package de.saschadoemer.agrirouter.protobuf.decoder.impl;
+
+import org.junit.jupiter.api.Test;
+
+class MessageOuterClassDecoderTest {
+
+    @Test
+    void givenValidInputDecodeMessageOutClassShouldReturnFormattedJson() {
+
+    }
+
+}
