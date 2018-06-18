4f6d061585b1ecc06454ff472b626a30e1984a44
Sascha Doemer <Sascha.Doemer@LMIS.de>
2018-06-18 23:37:21 +0200

    Draft.

--- protobuf-decoder/src/main/java/de/saschadoemer/agrirouter/protobuf/decoder/Decoder.java
+++ protobuf-decoder/src/main/java/de/saschadoemer/agrirouter/protobuf/decoder/Decoder.java
@@ -1,5 +1,7 @@
 package de.saschadoemer.agrirouter.protobuf.decoder;
 
+import java.io.ByteArrayInputStream;
+import java.util.Base64;
 import java.util.Optional;
 
 /**
@@ -13,4 +15,9 @@ public interface Decoder {
      * @return JSON
      */
     Optional<String> safeDecode(String utf8);
+
+    default ByteArrayInputStream decodeFromBase64(String base64EncodedValue) {
+        byte[] decodedBytes = Base64.getDecoder().decode(base64EncodedValue);
+        return new ByteArrayInputStream(decodedBytes);
+    }
 }
---
+++ protobuf-decoder/src/main/java/de/saschadoemer/agrirouter/protobuf/decoder/api/exceptions/CouldNotDecodeProtobufException.java
@@ -0,0 +1,8 @@
+package de.saschadoemer.agrirouter.protobuf.decoder.api.exceptions;
+
+public class CouldNotDecodeProtobufException extends RuntimeException{
+
+    public CouldNotDecodeProtobufException(Throwable cause) {
+        super(cause);
+    }
+}
