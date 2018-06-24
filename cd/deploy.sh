369724341e2c0afcbb0dfd9b1347b2cc39a664de
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2018-06-24 21:56:59 +0200

    Feature/publish to mvn central (#9)
    * Draft.
    
    * Draft.
    
    * Draft.
    
    * Draft.
    
    * Draft.
    
    * Draft.
    
    * Draft.
    
    * Draft.
    
    * Draft.
    
    * Draft.
    
    * Draft.
    
    * Draft.
    
    * Draft.
    
    * Draft.
    
    * Fix code style issues.
    
    * Documentation.
    
    * Fix code style issues.
    
    * Fix code style issues.
    
    * Documentation.
    
    * Draft.
    
    * Code style fixing.
    
    * Code style fixing.
    
    * Merge, signing and code style fixing.
    
    * SCM.
    
    * Meta information.
    
    * Meta information.
    
    * Code signing, deployment scripts.
    
    * Code signing, deployment scripts.

---
+++ cd/deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    mvn deploy -P sign,build-extras --settings cd/mvnsettings.xml
+fi
+
+if [ "$TRAVIS_BRANCH" = 'develop' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    mvn deploy -P build-extras --settings cd/mvnsettings.xml
+fi
