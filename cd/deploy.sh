bbad31fde7c4124ad08f19dad5594ffe6a2b5f4e
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2018-06-28 00:26:07 +0200

    Debugging.

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -P sign,build-extras --settings cd/mavensettings.xml
-fi
+#if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+#    mvn deploy -P sign,build-extras --settings cd/mavensettings.xml
+#fi
+#
+#if [ "$TRAVIS_BRANCH" = 'develop' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+#    mvn deploy -P build-extras --settings cd/mavensettings.xml
+#fi
 
-if [ "$TRAVIS_BRANCH" = 'develop' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -P build-extras --settings cd/mavensettings.xml
-fi
+mvn deploy -P build-extras --settings cd/mavensettings.xml
