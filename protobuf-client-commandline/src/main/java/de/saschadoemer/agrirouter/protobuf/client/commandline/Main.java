ba5d6f3a6af07ce22a876f4e268fda8a68b5417d
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2018-07-01 23:42:17 +0200

    Prepare.

---
+++ protobuf-client-commandline/src/main/java/de/saschadoemer/agrirouter/protobuf/client/commandline/Main.java
@@ -0,0 +1,66 @@
+package de.saschadoemer.agrirouter.protobuf.client.commandline;
+
+import de.saschadoemer.agrirouter.protobuf.client.commandline.handler.PasteInputHandler;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Main entry of the application.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        printWelcomeTitle();
+        boolean doNotLeaveApplication = true;
+        while (doNotLeaveApplication) {
+            printInputOptions();
+            int choice = readInputOption();
+            switch (choice) {
+                case 0:
+                    exit();
+                    doNotLeaveApplication = false;
+                    break;
+                case 1:
+                    PasteInputHandler pasteInputHandler = new PasteInputHandler();
+                    pasteInputHandler.handle();
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid option selected.");
+            }
+        }
+    }
+
+    private static void printWelcomeTitle() {
+        System.out.println("************************************************************************************");
+        System.out.println("*");
+        System.out.println("* AGRIROUTER TOOLSET / Decoding protobuf messages from the AR.");
+        System.out.println("*");
+        System.out.println("************************************************************************************");
+    }
+
+    private static void printInputOptions() {
+        System.out.println("[1] Paste input.");
+        System.out.println("[0] Quit.");
+    }
+
+    private static int readInputOption() {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do {
+            choice = scanner.nextInt();
+        }
+        while (!Arrays.asList(0, 1, 2).contains(choice));
+        return choice;
+    }
+
+    private static void exit() {
+        System.out.println("************************************************************************************");
+        System.out.println("*");
+        System.out.println("* AGRIROUTER TOOLSET / Thank you for using the decoder.");
+        System.out.println("* AGRIROUTER TOOLSET / Feel free to buy us a coffee [buymeacoff.ee/ks0iWGZlR] ;-)");
+        System.out.println("*");
+        System.out.println("************************************************************************************");
+    }
+
+}
---
+++ protobuf-client-commandline/src/main/java/de/saschadoemer/agrirouter/protobuf/client/commandline/helper/ResultPrinter.java
@@ -0,0 +1,11 @@
+package de.saschadoemer.agrirouter.protobuf.client.commandline.helper;
+
+public class ResultPrinter {
+
+    public void print(String result){
+        System.out.println("************************************************************************************");
+        System.out.println(result);
+        System.out.println("************************************************************************************");
+    }
+
+}
