b9361a237e500f968af3f06b3733c899c78f9e9a
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2018-07-11 11:15:23 +0200

    Maven refactoring.

--- protobuf-api/src/main/java/de/saschadoemer/agrirouter/protobuf/api/exceptions/UUIDUtils.java
+++
@@ -1,9 +0,0 @@
-package de.saschadoemer.agrirouter.protobuf.api.exceptions;
-
-public class UUIDUtils {
-
-    public static String generate(){
-        return java.util.UUID.randomUUID().toString();
-    }
-
-}
