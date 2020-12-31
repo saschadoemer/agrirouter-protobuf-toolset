558368a1a33a94452b269bedc28f74b10408f44d
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2020-12-31 07:26:12 +0100

    Migration of the build process. (#35)
    * Migration of the build process.
    
    * Remove unused JDKs.
    
    * Update Java version for CodeQL.
    
    * Update Java version for CodeQL.

--- cd/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -P sign,build-extras --settings cd/mavensettings.xml
-fi
-
-if [ "$TRAVIS_BRANCH" = 'develop' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -P build-extras --settings cd/mavensettings.xml
-fi
-
